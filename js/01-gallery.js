4dd88dfc88facaa5fd276c601ae89d7d3bffd27f
Anton Nikolaenko <anton.ck.ua@gmail.com>
2023-03-05 19:34:07 +0200

    gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,61 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+galleryItems.map(
+    ({ preview, original, description }) => {
+        const galleryItem = document.createElement('div');
+        galleryItem.classList.add('gallery__item');
+
+        const galleryLink = document.createElement('a');
+        galleryLink.classList.add('gallery__link');
+        galleryLink.href = `${original}`;
+          
+        const galleryImg = document.createElement('img');
+        galleryImg.classList.add('gallery__image');
+        galleryImg.src = `${preview}`;
+        galleryImg.dataset.source = `${original}`;
+        galleryImg.alt = `${description}`;
+
+        galleryLink.appendChild(galleryImg);
+        galleryItem.appendChild(galleryLink);
+        gallery.appendChild(galleryItem);
+    }
+
+)
+
+const onClick = event => {
+  event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+}
+
+const onEscapePress = (event) => {
+    const ESC_KEY_CODE = 'Escape';
+        if (event.code === ESC_KEY_CODE) {
+            instance.close();
+    }
+}
+    
+  
+const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`,{
+      
+        onShow: instance => {
+        window.addEventListener('keydown', onEscapePress);
+        },
+        
+        onClose: instance => {
+        window.removeEventListener('keydown', onEscapePress);
+      },
+    }
+  );
+  instance.show();
+
+
+}
+
+gallery.addEventListener('click', onClick);
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+galleryItems.map(
+    ({ preview, original, description }) => {
+        const galleryItem = document.createElement('a');
+        galleryItem.classList.add('gallery__item');
+        galleryItem.href = `${original}`;
+          
+        const galleryImg = document.createElement('img');
+        galleryImg.classList.add('gallery__image');
+        galleryImg.src = `${preview}`;
+        galleryImg.alt = `${description}`;
+
+        galleryItem.appendChild(galleryImg);
+        gallery.appendChild(galleryItem);
+    }
+
+)
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+
+
+
